8dc1be1ea5d41f6ae71da17d4e97816b66345c33
billy7175 <68532934+billy7175@users.noreply.github.com>
2022-02-13 18:26:40 +0900

    Create deposit, withdrawal DOM list

--- banklist/script.js
+++ banklist/script.js
@@ -73,4 +73,29 @@ const currencies = new Map([
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-/////////////////////////////////////////////////
+const displayMovements = function(movements){
+  containerMovements.innerHTML = ''
+  movements.forEach(function(mov, i){
+ 
+    const type = mov > 0 ? "deposit" : "withdrawal"
+    const html = `
+    <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
+          <div class="movements__value">${mov}</div>
+        </div>
+    `
+    containerMovements.insertAdjacentHTML('beforeend', html)
+  })
+}
+
+displayMovements(account1.movements)
+
+
+
+// for (const [i,movement] of movements.entries()){
+//   if(movement >0){
+//     console.log(`${i+1}: You deposited ${movement}`)
+//   } else {
+//     console.log(`${i+1}: You withdrew ${Math.abs(movement)}`)
+//   }
+// }
