e6ba7ef0b01b682339dc90d233ed57c16dc2df29
LAPTOP-C71E3L3P\Billy <whgus439@gmail.com>
2022-02-16 23:17:43 +0900

    Total Interest using Chain Method

--- banklist/script.js
+++ banklist/script.js
@@ -101,12 +101,18 @@ const calcDisplaySummary = function (){
   const incomes = movements
     .filter(mov => mov >0)
     .reduce((acc, mov) => acc+mov,0);
-    labelSumIn.textContent = `${incomes}$` 
+    labelSumIn.textContent = `${incomes}$`;
 
   const outs = movements
     .filter(mov => mov < 0)
     .reduce((acc, mov)=> acc + mov,0)
-    labelSumOut.textContent = `${Math.abs(outs)}$`
+    labelSumOut.textContent = `${Math.abs(outs)}$`;
+  
+  const interest = movements 
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit *1.2) / 100)
+    .reduce((acc, int) => acc + int, 0);
+    labelSumInterest.textContent = `${interest}`
 }
 calcDisplaySummary(account1.movements)
 
